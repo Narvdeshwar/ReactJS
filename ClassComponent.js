3f14e39206660e8c2f10439cec1de834afd41c36
Narvdeshwar <56790381+Narvdeshwar@users.noreply.github.com>
2022-11-25 16:56:34 +0530

    Add files via upload

---
+++ ClassComponent.js
@@ -0,0 +1,8 @@
+import React,{Component} from "react";
+export default class ClassComponent extends React.Component{
+    render(){
+        return(
+            <h1 className="text-primary">I am class Component</h1>
+        )
+    }
+}
---
+++ FunctionalComponent.js
@@ -0,0 +1,5 @@
+import React from "react"
+export default function FunctionalComponent(){
+    const name=React.createElement('h1',null,'Hi i am Ashrith')
+    return name;
+}
