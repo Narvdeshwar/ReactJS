83fa8785544416885df64bfa4ca934aa422356a3
narvdeshwar <sam.07soni@gmail.com>
2024-09-23 14:11:10 +0530

    counter app created

--- counterApp/src/App.jsx
+++ counterApp/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [counter, setCounter] = useState(0);
+  const increment = () => {
+    if(counter<20){
+      setCounter(counter + 1);
+    }
+    else{
+      alert("counter value can't exceed the value of 20")
+    }
+  };
+  const decrement = () => {
+    if(counter>0){
+      setCounter(counter - 1);
+    }
+    else{
+      alert("counter value can't below the negative")
+    }
+   
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>The Counter App</h2>
+      <h1>The counter value is = {counter}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
